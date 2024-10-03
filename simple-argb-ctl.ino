3b9ec2ed69ddf15b3a8dad01f8288e3385c4a5c5
maximilionus <maximilionuss@gmail.com>
2024-10-04 02:51:41 +0300

    New class based button processing system

--- simple-argb-ctl.ino
+++ simple-argb-ctl.ino
@@ -9,15 +9,92 @@
 #define ARGB_DATA_PIN 3
 #define LEDS_NUM 16
 
-CRGB leds[LEDS_NUM];
-bool buttonIsActive = false;
-unsigned long buttonLastPressTime = 0;
+enum class SystemMode {
+    Idle,
+    LEDConfigure_R,
+    LEDConfigure_G,
+    LEDConfigure_B
+};
 
-enum class ButtonStatus {
-    Released,
-    Press,
+enum class ButtonMode {
+    Idle,
+    PressDown,
+    PressUp,
     Hold
-} buttonStatus = ButtonStatus::Released;
+};
+
+class Button {
+private:
+    ButtonMode _mode;
+    unsigned long _lastPressTime;
+
+    void _setMode(ButtonMode mode) {
+        this->_mode = mode;
+
+        if (mode == ButtonMode::PressDown \
+            || mode == ButtonMode::PressUp)
+        {
+            this->_updateLastPress();
+            #ifdef DEBUG
+            Serial.println(F("[btn] Upd press time"));
+            #endif
+        }
+
+        #ifdef DEBUG
+        Serial.print(F("[btn] Mode: "));
+        Serial.println((uint16_t) mode);
+        #endif
+    }
+
+    void _updateLastPress() {
+        this->_lastPressTime = millis();
+    }
+
+public:
+    void setup() {
+        pinMode(BUTTON_PIN, INPUT_PULLUP);
+        this->_setMode(ButtonMode::Idle);
+    }
+
+    bool isPressed() {
+        // Button considered pressed on LOW because it's connected to ground
+        // with internal pullup.
+        return !digitalRead(BUTTON_PIN);
+    }
+
+    ButtonMode getMode() {
+        return this->_mode;
+    }
+
+    unsigned long getTimeFromLastPress() {
+        return millis() - this->_lastPressTime;
+    }
+
+    void tick() {
+        const ButtonMode currentMode = this->getMode();
+
+        if (this->isPressed()) {
+            if (currentMode == ButtonMode::Idle) {
+                this->_setMode(ButtonMode::PressDown);
+            } else if (currentMode == ButtonMode::PressDown) {
+                if (this->getTimeFromLastPress() > 250 && currentMode != ButtonMode::Hold) {
+                    this->_setMode(ButtonMode::Hold);
+                }
+            }
+       } else {
+            if (currentMode == ButtonMode::PressDown) {
+                this->_setMode(ButtonMode::PressUp);
+            } else if (currentMode != ButtonMode::Idle) {
+                this->_setMode(ButtonMode::Idle);
+            }
+       }
+    }
+};
+
+SystemMode systemMode;
+Button button;
+CRGB leds[LEDS_NUM];
+uint8_t rgbIter = 0;
 
 void setup() {
     #ifdef DEBUG
@@ -28,12 +105,15 @@ void setup() {
     #endif
 
     pinMode(LED_BUILTIN, OUTPUT);  // DEBUG
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
     pinMode(ARGB_DATA_PIN, OUTPUT);
+
+    button.setup();
 
     FastLED.addLeds<NEOPIXEL, ARGB_DATA_PIN>(leds, LEDS_NUM);
     FastLED.clear();
     FastLED.show();
+
+    systemMode = SystemMode::Idle;
 
     #ifdef DEBUG
     Serial.println(F("Setup done"));
@@ -41,40 +121,56 @@ void setup() {
 }
 
 void loop() {
-    updateButton();
+    button.tick();
+    //updateMode();
 }
 
-void updateButton() {
-    const bool buttonRead = digitalRead(BUTTON_PIN);
-
-    if (buttonRead == LOW) {
-        // Button considered pressed on LOW because it's connected to ground
-        // with internal pullup.
-        if (!buttonIsActive) {
-            buttonIsActive = true;
-            buttonStatus = ButtonStatus::Press;
-            buttonLastPressTime = millis();
+void updateMode() {
+    if (button.getMode() == ButtonMode::PressUp) {
+        if (systemMode == SystemMode::Idle \
+            || systemMode == SystemMode::LEDConfigure_B) // Cycle through R,G,B
+        {
+            systemMode = SystemMode::LEDConfigure_R;
+            fill_solid(leds, 4, CRGB::Blue);
+            FastLED.show();
 
             #ifdef DEBUG
-            Serial.println(F("[btn] Press"));
+            Serial.println(F("[SysM] Conf LED R"));
             #endif
-        } else {
-            if (millis() - buttonLastPressTime > 250 && buttonStatus != ButtonStatus::Hold) {
-                buttonStatus = ButtonStatus::Hold;
-                #ifdef DEBUG
-                Serial.println(F("[btn] Hold"));
-                #endif
-            }
-        }
-   } else {
-        if (buttonIsActive) {
-            buttonIsActive = false;
-            buttonStatus = ButtonStatus::Released;
+        } else if (systemMode == SystemMode::LEDConfigure_R) {
+            systemMode = SystemMode::LEDConfigure_G;
+            fill_solid(leds, 4, CRGB::Blue);
+            FastLED.show();
+
             #ifdef DEBUG
-            Serial.println(F("[btn] Released"));
+            Serial.println(F("[SysM] Conf LED G"));
+            #endif
+        } else if (systemMode == SystemMode::LEDConfigure_G) {
+            systemMode = SystemMode::LEDConfigure_B;
+            fill_solid(leds, 4, CRGB::Blue);
+            FastLED.show();
+
+            #ifdef DEBUG
+            Serial.println(F("[SysM] Conf LED B"));
             #endif
         }
-   }
+
+        // Hacky way to prevent ghost input on single press
+        //delay(250);
+    } else if (button.getMode() == ButtonMode::Hold) {
+        if (systemMode == SystemMode::LEDConfigure_R) {
+
+        }
+    } else if (button.getMode() == ButtonMode::Idle) {
+        if (button.getTimeFromLastPress() >= 5000 \
+            && systemMode != SystemMode::Idle)
+        {
+            systemMode = SystemMode::Idle;
+            #ifdef DEBUG
+            Serial.println(F("[SysM] Idle"));
+            #endif
+        }
+    }
 }
 
 // Test the RGB controls
